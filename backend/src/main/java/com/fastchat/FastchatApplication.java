df0e13951d9c308f7ba208416fa66eec95f0993f
GreenBudgie <forestengine@yandex.ru>
2022-01-26 19:29:10 +0300

    Initial commit

---
+++ backend/src/main/java/com/fastchat/FastchatApplication.java
@@ -0,0 +1,13 @@
+package com.fastchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastchatApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fastchat/FastchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
